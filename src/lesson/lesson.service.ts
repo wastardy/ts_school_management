5bd668fa35344126504f5f3565f9f7f9ddd5c3f3
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-26 17:07:44 +0200

    add getLesson method to LessonService with NotFoundException handling

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -1,15 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Lesson } from './lesson.entity';
 import { Repository } from 'typeorm';
 import { CreateLessonDto } from './dtos/create-lesson.dto';
 import { v4 as uuid } from 'uuid';
+import errorConstants from '../constants/error.constants';
 
 @Injectable()
 export class LessonService {
   constructor(
     @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
   ) {}
+
+  async getLesson(id: string): Promise<Lesson> {
+    const lesson = await this.lessonRepository.findOneBy({ id });
+
+    if (!lesson) {
+      throw new NotFoundException(errorConstants.LESSON_NOT_FOUND);
+    }
+
+    return lesson;
+  }
 
   async createLesson(createLesson: CreateLessonDto): Promise<Lesson> {
     const { name, startDate, endDate } = createLesson;
