b49532b780d0ef6dc9525807c881320f1c09e71a
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-26 16:48:10 +0200

    implement createLesson mutation in LessonResolver with arguments and integrate LessonService

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,8 +1,11 @@
-import { Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
 
 @Resolver(() => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
+
   @Query(() => LessonType)
   lesson() {
     return {
@@ -14,5 +17,11 @@ export class LessonResolver {
   }
 
   @Mutation(() => LessonType)
-  createLesson() {}
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    return this.lessonService.createLesson({ name, startDate, endDate });
+  }
 }
