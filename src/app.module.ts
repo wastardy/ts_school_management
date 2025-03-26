85b307bff83e4ce750152ebd32612eb8cd47866d
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-26 15:53:39 +0200

    configure TypeORM with MongoDB connection in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { LessonModule } from './lesson/lesson.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://mongoadmin:mongoadmin@localhost:27017',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
       driver: ApolloDriver,
