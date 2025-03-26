39e3f67f95d2903f39aa60afce99db0ac4c26128
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-26 18:58:47 +0200

    add Student entity, import it to student module and set as entity in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { LessonModule } from './lesson/lesson.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LessonModule } from './lesson/lesson.module';
 import { Lesson } from './lesson/lesson.entity';
 import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import { StudentModule } from './student/student.module';
       url: 'mongodb://mongoadmin:mongoadmin@localhost:27017',
       synchronize: true,
       useUnifiedTopology: true,
-      entities: [Lesson],
+      entities: [Lesson, Student],
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
