6dfac7771ffb9e08fff04c8346fe660cffe89933
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-26 19:22:07 +0200

    add getStudent method to StudentService for retrieving a student by ID with error handling

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Student } from './student.entity';
+import { Repository } from 'typeorm';
+import errorConstants from 'src/constants/error.constants';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+  ) {}
+
+  async getStudent(id: string): Promise<Student> {
+    const student = await this.studentRepository.findOneBy({ id });
+
+    if (!student) {
+      throw new NotFoundException(errorConstants.STUDENT_NOT_FOUND);
+    }
+
+    return student;
+  }
+}
