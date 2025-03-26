79498c07edbacb8f2c3c32f4d2fa530766d79078
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-26 14:07:19 +0200

    init NestJS application with GraphQL support and basic configuration

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+  ],
+})
+export class AppModule {}
